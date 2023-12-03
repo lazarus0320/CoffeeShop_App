36575f9ec4c5e0576996e26f3a47bfceb9980052
민지훈 <84646738+lazarus0320@users.noreply.github.com>
2023-12-03 16:18:52 +0900

    feat: 팝업창 수량 조절 및 가격 적용 기능 추가

--- app/src/main/java/com/example/coffeeshop/fragment/itemDialog.kt
+++ app/src/main/java/com/example/coffeeshop/fragment/itemDialog.kt
@@ -14,29 +14,81 @@ class ItemDialog(context: Context, private val item: CoffeeTeaItem) : Dialog(con
     private lateinit var imageView: ImageView
     private lateinit var textViewName: TextView
     private lateinit var textViewPrice: TextView
+    private lateinit var textViewQuantity: TextView
+    private lateinit var btnIncrease: Button
+    private lateinit var btnDecrease: Button
     private lateinit var shutdownClick: Button
 
-    @SuppressLint("MissingInflatedId")
+    private var quantity = 1
+
+    companion object {
+        const val DEFAULT_QUANTITY = 1
+    }
+
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_item_details)
 
+        initializeViews()
+        setupListeners()
+
+        updateViews()
+    }
+
+    private fun initializeViews() {
         imageView = findViewById(R.id.imageViewItemPopup)
         textViewName = findViewById(R.id.itemNamePopup)
         textViewPrice = findViewById(R.id.itemPricePopup)
-
-        imageView.setImageResource(item.imageResource)
-        textViewName.text = item.name
-        textViewPrice.text = item.price.toString()
-
+        textViewQuantity = findViewById(R.id.quantityValue)
+        btnIncrease = findViewById(R.id.btnIncrease)
+        btnDecrease = findViewById(R.id.btnDecrease)
         shutdownClick = findViewById(R.id.btn_shutdown)
-        shutdownClick.setOnClickListener {
-            shutDownClick()
+    }
+
+    private fun setupListeners() {
+        btnIncrease.setOnClickListener {
+            increaseQuantity()
+            updateQuantityAndPrice()
         }
 
+        btnDecrease.setOnClickListener {
+            decreaseQuantity()
+            updateQuantityAndPrice()
+        }
 
+        shutdownClick.setOnClickListener {
+            dismiss()
+        }
     }
-    private fun shutDownClick() {
-        dismiss()
+
+    private fun increaseQuantity() {
+        quantity++
+    }
+
+    private fun decreaseQuantity() {
+        if (quantity > DEFAULT_QUANTITY) {
+            quantity--
+        }
+    }
+
+    private fun updateQuantityAndPrice() {
+        updateQuantityText()
+        updatePriceText()
+    }
+
+    private fun updateViews() {
+        imageView.setImageResource(item.imageResource)
+        textViewName.text = item.name
+        updateQuantityAndPrice()
+    }
+
+    private fun updateQuantityText() {
+        textViewQuantity.text = quantity.toString()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updatePriceText() {
+        textViewPrice.text = "${item.price * quantity}원"
     }
 }
